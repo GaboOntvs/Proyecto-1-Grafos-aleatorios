a231e803bd18ea0ae1ccd75cdb720b9b0112e9b6
GaboOntvs <55205768+GaboOntvs@users.noreply.github.com>
2019-10-16 14:16:13 -0500

    Add files via upload

--- Grafo/src/grafo/Nodo.java
+++ Grafo/src/grafo/Nodo.java
@@ -1,17 +1,37 @@
 
 package grafo;
 
+import java.util.LinkedList;
+import java.util.HashMap;
+
 public class Nodo {
     //Atributos: Identificador de Nodo
     String IDnodo;
     double x,y;
     int grado;
-
+    //Utiles para los árboles BFS y DFS
+    boolean explorado; //todos los nodos en automático son "NO explorados"
+    LinkedList <Nodo> adyList = new LinkedList <>();
+    //Utiles para Dijkstra
+    LinkedList <contenedor> adyList2 = new LinkedList <>();
+    float dist; //distancia hacia el nodo, se inicializan en infinito.
+    Nodo prev; //Nodo previo "u" (u,v), para construir el arbol de Dijkstra.
+    //float w; //Peso de la arista (u,v), donde "v" es este mismo Nodo y "u" el anterior
+    
+    //Métodos de la clase Nodo
+    public Nodo ()
+    {
+        //Constructor vacío.
+        explorado=false;
+        dist=Float.POSITIVE_INFINITY;
+    }
     public Nodo (String name, int i)
     {
         /*Constructor para instancias de clase Nodo que
         únicamente crea al objeto con un ID único*/
         IDnodo=name+i;
+        explorado=false;
+        dist=Float.POSITIVE_INFINITY;
     }
     public Nodo (String name, int i, double X, double Y)
     {
@@ -20,6 +40,8 @@ public class Nodo {
         en un espacio unitario*/
         IDnodo=name+i;
         x=X;
-        y=Y;        
+        y=Y;    
+        explorado=false;
+        dist=Float.POSITIVE_INFINITY;
     }
 }
---
+++ Grafo/src/grafo/contenedor.java
@@ -0,0 +1,22 @@
+
+package grafo;
+
+/**
+ *
+ * @author F5-573
+ */
+ public class contenedor
+{
+    /*Objeto que contiene el nodo adyacente con el respectivo peso
+    de la arista que está incidiendo en él. Puede haber más de una
+    arista incidente, por lo que un solo nodo puede manejar el peso
+    de todas las aristas que inciden en él.*/
+    Nodo nodAd;
+    float w;
+    int ID;
+    //constructor
+    public contenedor (int id)
+    {
+        ID=id; //usar un ID para identificar multiples contenedores
+    }
+}
